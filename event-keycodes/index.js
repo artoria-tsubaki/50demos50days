f5aa2d0794d512c06a9bcb81ef23e243ef780b68
pycd <1248353039@qq.com>
2022-06-21 09:12:42 +0800

    event-keycodes

---
+++ event-keycodes/index.js
@@ -0,0 +1,24 @@
+window.addEventListener('keydown', keyDownHandler)
+
+const container = document.querySelector('.container')
+
+function keyDownHandler (e) {
+  // 清空 container
+  container.innerHTML = ''
+  // 插入卡片
+  const html = `<div class="entered">
+    <div class="key-item">
+      <div class="title">event.key</div>
+      <div class="card">${e.key}</div>
+    </div>
+    <div class="key-item">
+      <div class="title">event.keyCode</div>
+      <div class="card">${e.keyCode}</div>
+    </div>
+    <div class="key-item">
+      <div class="title">event.code</div>
+      <div class="card">${e.code}</div>
+    </div>
+  </div>`
+  container.innerHTML = html
+}
