c0e1a41a8adbd31a6a8e1a09201a62c4c26dfd54
pycd <1248353039@qq.com>
2022-07-12 10:18:02 +0800

    double-vertical-slider

--- double-vertical-slider/index.js
+++ double-vertical-slider/index.js
@@ -1,38 +1,31 @@
-const leftSlide = document.querySelector('.left-slide')
-const rightSlide = document.querySelector('.right-slide')
-const downBtn = document.querySelector('.down-btn')
-const upBtn = document.querySelector('.up-btn')
-const slideLength = document.querySelectorAll('.left-slide>div').length
+const sliderContainer = document.querySelector('.container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-btn')
+const downButton = document.querySelector('.down-btn')
+const slidesLength = slideRight.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
-rightSlide.style.transform = `translateY(${(slideLength - 1) * 100}vh)` // -300 -- 0
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
-downBtn.addEventListener('click', downBtnHandler)
-upBtn.addEventListener('click', upBtnHandler)
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
 
-function downBtnHandler() {
-    activeSlideIndex--
-    if(activeSlideIndex < 0) {
-        activeSlideIndex = slideLength - 1
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
     }
-    leftSlideContainerMove()
-    rightSlideContainerMove()
-}
 
-function upBtnHandler() {
-    activeSlideIndex++
-    if(activeSlideIndex > slideLength - 1) {
-        activeSlideIndex = 0
-    }
-    leftSlideContainerMove()
-    rightSlideContainerMove()
-}
-
-function leftSlideContainerMove() {
-    leftSlide.style.transform = `translateY(${activeSlideIndex * 100}vh)`
-}
-
-function rightSlideContainerMove() {
-    rightSlide.style.transform = `translateY(${(slideLength - 1 - activeSlideIndex) * 100}vh)`
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
