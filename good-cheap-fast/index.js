05350ac13bef30eb4995bf183c4e01c04cca10cb
pycd <1248353039@qq.com>
2022-07-19 09:49:40 +0800

    good-cheap-fast

---
+++ good-cheap-fast/index.js
@@ -0,0 +1,26 @@
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+const toggles = document.querySelectorAll('.toggle')
+
+let checkedValue = []
+
+toggles.forEach(toggle => toggleEventInit(toggle))
+
+function toggleEventInit (toggle) {
+  toggle.addEventListener('change', (e) => doTheTrick(e.target))
+}
+
+function doTheTrick (theClickedOne) {
+  let index = checkedValue.findIndex(item => item === theClickedOne.id)
+
+  if (index !== -1) {
+    checkedValue.splice(index, 1)
+  } else {
+    checkedValue.push(theClickedOne.id)
+  }
+
+  if (checkedValue.length >= 3) {
+    document.querySelector(`#${checkedValue.shift()}`).checked = false
+  }
+}
